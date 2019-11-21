c84e3544c653069743cb2ad8fba4ad609b3ef6f8
Blast0ffGaming <blasty0ff@gmail.com>
2019-11-21 22:15:49 +0800

    gud

--- server.py
+++ server.py
@@ -40,4 +40,6 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text="ur gud, I luv you~"))
+
+#event.message.text
